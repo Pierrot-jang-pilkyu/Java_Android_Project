63d3ea777ce592cd71c82eaaa48b1f6bab66a93d
Pierrot-Jang-pilkyu <jangpilkyu1@gmail.com>
2021-05-19 21:08:41 +0900

    May. 19. 2021 1 commit
    1. real DP - use the memoization!
    
    1. DP의 목적에 맞게 큰 문제는 작은 문제들로 나뉘어 있으며, 중복되는 계산을 막기 위해 값을
    저장하여 해당하는 작은 문제의
    값을 바로 불러옴으로서 시간을
    절약 할 수 있다.

--- English_Quiz/1__Study/algorithm/dp/SugaBag.java
+++ English_Quiz/1__Study/algorithm/dp/SugaBag.java
@@ -1,71 +1,56 @@
+// Default Value : MN[3] = 1, MN[5] = 1 -> Using memoization DP !!!
+
 package algorithm.dp;
 import java.util.Scanner;
 
-class Pair<T1, T2> {
-    private T1 first;
-    private T2 second;
-
-    public Pair(T1 f, T2 s){
-        this.first = f;
-        this.second = s;
-    }
-
-    public void set(T1 f, T2 s){
-        this.first = f;
-        this.second = s;
-    }
-
-    public T1 getfirst(){
-        return this.first;
-    }
-
-    public T2 getsecond(){
-        return this.second;
-    }
-}
-
 public class SugaBag {
-    static int result = 0;
+    static int N_MAX = 5001;
+    static int N, result;
+    static int []MN = new int[N_MAX];  // MinNumber, Memoization
 
-    static void choice(Pair<Integer, Integer> n){
-        if(result == 0){
-            if(n.getfirst() < 3){
-                if(n.getfirst() == 2) return;
-                if(n.getfirst() == 1) return;
-                if(n.getfirst() == 0){
-                    result = n.getsecond();
-                }
-                if(n.getfirst() < 0) return;
-            }
-            else{
-                Pair<Integer, Integer> temp = new Pair<>(0 ,0);
-                temp.set(n.getfirst(), n.getsecond());
+    static int min(int a, int b) { if( (char)a < (char)b ) return a; return b; }
 
-                n.set(n.getfirst() - 5, n.getsecond() + 1);
-                choice(n);
+    static void Input(){
 
-                n.set(temp.getfirst() - 3, temp.getsecond() + 1);
-                choice(n);
-            }
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        sc.close();
+
+        MN[0] = 0;
+        MN[1] = -1;
+        MN[2] = -1;
+        MN[3] = 1;
+        MN[4] = -1;
+        MN[5] = 1;
+    }
+
+    static void Solution() {
+        
+        for(int i = 6; i <= N; i++){
+
+            if((MN[i - 3] == -1) && (MN[i - 5] == -1)) { MN[i] = -1; continue; }
+            MN[i] = min(MN[i - 3], MN[i - 5]) + 1;
 
         }
+
+        result = MN[N];
+
+        System.out.println(result);
+    }
+
+    static void Solve(){
+
+        Input();
+
+        Solution();
+
     }
 
     public static void main(String[] args) {
 
-        
-        Pair<Integer, Integer> n = new Pair<>(0 ,0);
-        
-        Scanner sc = new Scanner(System.in);
-
-        n.set(sc.nextInt(), n.getsecond());
-
-        choice(n);
-
-        if(result == 0) System.out.println(-1);
-        else System.out.println(result);
-
-        sc.close();
+        Solve();
         
     }
 }
